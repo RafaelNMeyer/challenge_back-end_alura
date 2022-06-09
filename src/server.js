b4afb2d3f98586480ba0df13c3cdf628d76d22f1
RafaelNMeyer <rafaelniederauer07@gmail.com>
2022-06-09 16:40:47 -0300

    port error fix

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import './setup.js';
 import app from './app.js'
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
